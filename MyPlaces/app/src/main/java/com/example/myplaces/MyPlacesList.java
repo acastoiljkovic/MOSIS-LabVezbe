ddc1711e41e77cc732b033686bb2689f523893d8
Aca <aca.stoiljkovic@elfak.rs>
2020-04-11 21:46:17 +0200

    Odradjena 2 lab vezba

---
+++ MyPlaces/app/src/main/java/com/example/myplaces/MyPlacesList.java
@@ -0,0 +1,67 @@
+package com.example.myplaces;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class MyPlacesList extends AppCompatActivity {
+    ArrayList<String> places;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_places_list);
+
+        // Ako zatreba floating button
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action",null).show();
+//            }
+//        });
+
+        places = new ArrayList<String>();
+        places.add("Tvrdjava");
+        places.add("Cair");
+        places.add("Park Svetog Save");
+        places.add("Trg Kralja MIilana");
+        ListView myPlacesList = (ListView)findViewById(R.id.my_places);
+        myPlacesList.setAdapter((new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,places)));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_my_places_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.show_map_item){
+            Toast.makeText(this,"Show Map", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.new_place_item){
+            Toast.makeText(this,"New Place", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.about_item){
+            Intent i = new Intent(this, About.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
